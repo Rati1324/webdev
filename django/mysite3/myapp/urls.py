316f5a2a5f442b4056b6a6fffd49ac445d628037
Rati1324 <rati.mtsituri@gau.edu.ge>
2023-01-13 19:01:17 +0400

    _

--- django/mysite3/myapp/urls.py
+++ django/mysite3/myapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register', views.register, name="register"),
     path('', include("django.contrib.auth.urls")),
     path('create_post', views.create_post),
+    path('post/<int:post_id>', views.post_details),
 ]
--- django/mysite3/myapp/views.py
+++ django/mysite3/myapp/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from .forms import RegisterForm
 from .models import Post
+from django.contrib.auth.decorators import login_required
 
 def home(request):
-    return render(request, "home.html")
+    posts = Post.objects.all()
+    return render(request, "home.html", {"posts": posts})
 
 def register(request):
     if request.method == "POST":
@@ -16,10 +18,17 @@ def register(request):
         form = RegisterForm()
     return render(request, "register.html", {"form": form})
 
+@login_required(login_url="/login/")
 def create_post(request):
-    if request.method == "GET":
+    if request.method == "GET" and request.user.is_authenticated:
         return render(request, "create_post.html")
 
     post = Post(title = request.POST["title"], body = request.POST["body"], user_id = request.user.id)
     post.save()
     return redirect("/")
+
+
+@login_required(login_url="/login/")
+def post_details(request, post_id):
+    post = Post.objects.all().get(id=post_id)
+    return render(request, "post.html", {"post": post})
